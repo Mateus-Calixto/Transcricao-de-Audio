fb66381f1119fc190b022cd625486c62f52dcc1b
Mateus Vinicius Calixto <mc3230855@gmail.com>
2025-02-02 23:23:36 -0300

    postagem do projeto

---
+++ trascrever.py
@@ -0,0 +1,12 @@
+
+
+
+import whisper
+import warnings
+
+warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
+modelo = whisper.load_model("medium")
+
+resposta = modelo.transcribe("C:/Users/mc323/Documents/Projetos-github/Transcrevendo-audio/audios five/pregação-krisman-aprimorado.mp3")
+
+print(resposta['text'])
